e71f667df22b64c5d66a9185619664ffbeaa1b9b
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-01-25 18:46:48 -0300

    refactor: requisito 3 - reduz a complexidade da implementacao

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,6 @@
+# from challenges.utils.my_sort_function import my_sort_function
+
+
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
     raise NotImplementedError
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,17 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
+    if low_index > high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
 
-    while high_index > low_index <= len(word) // 2:
-        if word[low_index] != word[high_index]:
-            return False
-        else:
-            return (
-                True
-                and is_palindrome_recursive(
+    return is_palindrome_recursive(
                         word,
                         low_index + 1,
                         high_index - 1
                     )
-            )
-    return True
