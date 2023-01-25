583b404ed5fc96b905f2637dccfe81fadf0d99fd
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-01-25 16:05:40 -0300

    feat: requisito 1 - implementa funcao "study_schedule" recebe uma lista de tuplas contendo valores iniciais e finais em que usuarios estao usando a internet cono primeiro parametro, e um valor inteiro para ser pesquisado quantas pessoas estao utilizando naquele horario retorna a quantidade de usuarios ativos na hora referente a aquele valor

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    active_users = 0
+    try:
+        for initial, final in permanence_period:
+            if initial <= target_time and final >= target_time:
+                active_users += 1
+        return active_users
+    except (TypeError, ValueError):
+        return None
