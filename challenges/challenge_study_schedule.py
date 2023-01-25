22a5e90d66cb2e880e929b711553959259054767
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-01-25 16:59:24 -0300

    feat: requisito 2 - adiciona testes para a funcao "encrypt" testa se a funcao esta devolvendo o resultado esperado e se quando recebe valores invalidos envia o erro esperado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     active_users = 0
     try:
         for initial, final in permanence_period:
-            if initial <= target_time and final >= target_time:
+            if initial <= target_time <= final:
                 active_users += 1
         return active_users
     except (TypeError, ValueError):
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    correct_word = "potato"
+    assert encrypt_message(correct_word, 2) == "otat_op"
+    assert encrypt_message(correct_word, 4) == "ot_atop"
+    assert encrypt_message(correct_word, 10) == "otatop"
+    assert encrypt_message(correct_word, 3) == "top_ota"
+
+    with pytest.raises(TypeError, match=r"tipo inválido para key"):
+        encrypt_message(correct_word, "kiko?")
+
+    with pytest.raises(TypeError, match=r"tipo inválido para message"):
+        encrypt_message(55, 4)
