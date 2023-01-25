21a021d8808bb3dc6a19a1a7d28070477ba7cd65
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-01-25 17:41:53 -0300

    feat: requisito 3 - implementa a funcao "is_palindrome_recursive" recebe como parametro uma string, um valor do index inicial e um valor do index final e de forma recursiva verifica se essa string é ou nao um palindromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    while high_index > low_index <= len(word) // 2:
+        if word[low_index] != word[high_index]:
+            return False
+        else:
+            return (
+                True
+                and is_palindrome_recursive(
+                        word,
+                        low_index + 1,
+                        high_index - 1
+                    )
+            )
+    return True
