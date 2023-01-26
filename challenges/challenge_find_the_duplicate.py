b863c8112d219585a9876aca48ed524bc771ee97
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-01-26 17:53:05 -0300

    feat: requisito requisito 5 - implementa a funcao "find_duplicate" recebe uma lista de numeros como parametro e verifica se tem algum numero repetido, caso encontre, retornara o numero, caso nao encontre ou receber valores invalidos retornara "False"

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,18 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_invalid_num(num):
+    if type(num) is not int or num < 0:
+        return True
+    else:
+        return False
+
+
+def find_duplicate(nums, start_index=0):
+    length = len(nums)
+    if start_index >= length or length == 1:
+        return False
+
+    for i in range(start_index + 1, length):
+        if is_invalid_num(nums[i]) or is_invalid_num(nums[start_index]):
+            return False
+        if nums[start_index] == nums[i]:
+            return nums[start_index]
+    return find_duplicate(nums, start_index + 1)
